9adde2648c47b9d66a86b617b2e977b43887ecd6
IvanStacy <139801928+IvanStacy@users.noreply.github.com>
2023-12-23 16:59:24 +0000

    done witn favorites

--- src/models.py
+++ src/models.py
@@ -2,11 +2,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+FavoritePeople = db.Table("FavoritePeople", 
+db.Column("userId", db.Integer, db.ForeignKey("user.id"), primary_key=True),
+db.Column("peopleId", db.Integer, db.ForeignKey("people.id"), primary_key=True)
+)
+FavoritePlanets = db.Table("FavoritePlanets", 
+db.Column("userId", db.Integer, db.ForeignKey("user.id"), primary_key=True),
+db.Column("planetsId", db.Integer, db.ForeignKey("planets.id"), primary_key=True)
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favoritePeople = db.relationship("People", secondary = "FavoritePeople")
+    favoritePlanets = db.relationship("Planets", secondary = "FavoritePlanets")
 
     def __repr__(self):
         return '<User %r>' % self.email
@@ -14,8 +25,11 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email
+            "email": self.email,
+            "favoritePeople": list(map(lambda x: x.serialize(), self.favoritePeople)),
+            "favoritePlanets": list(map(lambda x: x.serialize(), self.favoritePlanets))
             # do not serialize the password, its a security breach
+
         }
 
 class People(db.Model):
@@ -24,6 +38,7 @@ class People(db.Model):
     eye_color = db.Column(db.String(80), unique=False, nullable=True)
     gender = db.Column(db.String(80), unique=False, nullable=True)
     birth_year = db.Column(db.String(120), unique=False, nullable=False)
+    # favorites = db.relationship("Favorites",backref = "People")
 
     def __repr__(self):
         return '<People %r>' % self.name
@@ -35,6 +50,7 @@ class People(db.Model):
             "eye_color": self.eye_color,
             "gender": self.gender,
             "birth_year": self.birth_year
+            
             # do not serialize the password, its a security breach
         }
 
@@ -45,6 +61,7 @@ class Planets(db.Model):
     population = db.Column(db.Integer, unique=False, nullable=True)
     orbital_period = db.Column(db.Float, unique=False, nullable=False)
     gravity = db.Column(db.String(120), unique=False, nullable=False)
+    # favorites = db.relationship("Favorites",backref = "Planets")
 
     def __repr__(self):
         return '<Planets %r>' % self.name
@@ -59,3 +76,22 @@ class Planets(db.Model):
             "gravity": self.gravity
             # do not serialize the password, its a security breach
         }
+
+# class Favorites(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     userId = db.Column(db.Integer, db.ForeignKey("user.id"))
+#     characterId = db.Column(db.Integer, db.ForeignKey("character.id"))
+#     planetId = db.Column(db.Integer, db.ForeignKey("planet.id"))
+
+#     def __repr__(self):
+#         return '<Favorites %r>' % self.name
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "userId": self.userId,
+#             "charactesId": self.characterId,
+#             "planetId": self.planetId
+#             # do not serialize the password, its a security breach
+
+#         }
